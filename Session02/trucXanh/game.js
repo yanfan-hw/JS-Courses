d6fab72c2a127a587b266d131c091fbe3e2b6cb1
YanFan Hw <yanfanhw@gmail.com>
2022-04-16 01:51:05 +0700

    Create board game

--- Session02/trucXanh/game.js
+++ Session02/trucXanh/game.js
@@ -1,32 +1,39 @@
 const boardGame = document.createElement('div')
-const boardGame_style = {
+const BOARDGAME_STYLE = {
     backgroundColor: 'pink',
     position: 'relative',
     margin: '0 auto',
-    width: '700px',
-    height: '800px'
+    width: '560px',
+    height: '600px'
 }
 
-Object.assign(boardGame.style, boardGame_style)
+Object.assign(boardGame.style, BOARDGAME_STYLE)
 document.body.appendChild(boardGame)
 
 const arrCards = []
-const card = document.createElement('div')
-const card_style = {
+
+const CARD_STYLE = {
     position: 'absolute',
     fontSize: '16px',
     width: '100px',
     height: '100px',
-    top: '0',
-    left: '0',
+    top: '',
+    left: '',
     borderRadius: '5px',
-    backgroundColor: 'red'
+    backgroundColor: 'hotpink'
 }
-Object.assign(card.style, card_style)
+let cardLeft = 10, cardTop = 10
 
 for (let i = 0; i < 20; i++) {
-    
+    const card = document.createElement('div')
+    CARD_STYLE.left = `${cardLeft}px`
+    CARD_STYLE.top = `${cardTop}px`
+    Object.assign(card.style, CARD_STYLE)
     boardGame.appendChild(card)
+    if ((i + 1) % 5 === 0) {
+        cardLeft = 10
+        cardTop += 110
+    } else {
+        cardLeft += 110
+    }
 }
-
-console.log(card.style)
