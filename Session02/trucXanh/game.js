fbe250e4fbde4c5427da025512a8eef2eb6254fb
YanFan Hw <yanfanhw@gmail.com>
2022-04-16 12:26:18 +0700

    Add card pokemon

--- Session02/trucXanh/game.js
+++ Session02/trucXanh/game.js
@@ -1,10 +1,10 @@
 const boardGame = document.createElement('div')
 const BOARDGAME_STYLE = {
-    backgroundColor: 'pink',
+    background: 'radial-gradient(circle,#020024 0,#090979 35%,#00d4ff 100%)',
     position: 'relative',
     margin: '0 auto',
     width: '560px',
-    height: '600px'
+    height: '450px'
 }
 
 Object.assign(boardGame.style, BOARDGAME_STYLE)
@@ -12,7 +12,7 @@ document.body.appendChild(boardGame)
 
 const BASE_URL =
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white";
-const pokemonImg = [
+const pokemonImgs = [
     { id: "poke-0", image: `${BASE_URL}/1.png` },
     { id: "poke-1", image: `${BASE_URL}/2.png` },
     { id: "poke-2", image: `${BASE_URL}/3.png` },
@@ -25,6 +25,21 @@ const pokemonImg = [
     { id: "poke-9", image: `${BASE_URL}/10.png` }
 ]
 
+var pokemons = []
+
+for(let i =0; i < 10; i++) {
+    let pokemon = {
+        id: i,
+        matched: false,
+        image: `${pokemonImgs[i].image}`
+    }
+    pokemons.push(pokemon)
+    pokemons.push(pokemon)
+}
+pokemons = pokemons.sort(function () {
+    return Math.random() - 0.5
+})
+
 const CARD_STYLE = {
     position: 'absolute',
     fontSize: '16px',
@@ -33,8 +48,14 @@ const CARD_STYLE = {
     top: '',
     left: '',
     borderRadius: '5px',
-    backgroundColor: 'hotpink'
+    border: '2px solid #fff',
+    backgroundColor: '#26a0da',
+    backgroundPosition: '50%',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundImage: `url(${pokemonImgs[8].image})`
 }
+// console.log(CARD_STYLE.backgroundImage)
 let cardLeft = 10, cardTop = 10
 
 for (let i = 0; i < 20; i++) {
@@ -43,6 +64,8 @@ for (let i = 0; i < 20; i++) {
     CARD_STYLE.top = `${cardTop}px`
     Object.assign(card.style, CARD_STYLE)
     boardGame.appendChild(card)
+    // console.log(card.style.backgroundPosition)
+    // pokemon.push(card)
     if ((i + 1) % 5 === 0) {
         cardLeft = 10
         cardTop += 110
